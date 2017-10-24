6ea0c230db4e1441e15d74b9f30660b1d753cb07
devsheder <devsheder@gmail.com>
2017-10-24 09:07:40 +0200

    feat:call the service to get HTML page

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-const https = require('https');
-const parse5 = require('parse5');
+const FFFPageService = require('./services/https/FFFPage.service');
 
-var options = {
-    host: 'www.fff.fr',
-    method: 'GET',
-    path:'/la-vie-des-clubs/169290/resultats',
-    headers: {
-        "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:16.0) Gecko/20100101 Firefox/16.0 FirePHP/0.6"
-    }
-};
-
-let htmlData = "";
-https.get(options, (res) => {
-
-  res.on('data', (d) => {
-    htmlData += d;
-  });
-
-  res.on('end', () => {
-    let parseResult = parse5.parse(htmlData);
-  });
-
-}).on('error', (e) => {
-  console.error(e);
+FFFPageService.getHtmlPage('/la-vie-des-clubs/169290/resultats').then((html) => {
+  console.log(html);
+}).catch((error) => {
+  console.log(error);
 });
