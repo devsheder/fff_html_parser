4afce30a1ba71999b304220042c0cb37aec73cea
devsheder <devsheder@gmail.com>
2017-10-25 07:27:26 +0200

    feat:persists the results in file and check if the results have already been fetch from website

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const FFFPageService = require('./services/https/FFFPage.service');
+const fs = require("fs");
 
-FFFPageService.getHtmlPage('/la-vie-des-clubs/169290/resultats').then((html) => {
-  console.log(html);
-}).catch((error) => {
-  console.log(error);
-});
+const FFFPageService = require('./services/https/FFFPage.service');
+const FFFParsingService = require('./services/parsing/FFFParsing.service');
+
+const lastWeekEndResultsFileName = "lastWeekEndResults.json";
+
+if (fs.existsSync(lastWeekEndResultsFileName)) {
+  console.log("The resultats of last weekend have already been fetched");
+  // Let's check and get missing scores
+  // Building array of promises
+  let lastWeekEndResults = JSON.parse(fs.readFileSync(lastWeekEndResultsFileName));
+} else {
+  console.log("Getting the results page");
+  FFFPageService.getHtmlPage('/la-vie-des-clubs/169290/resultats').then((html) => {
+    let results = FFFParsingService.parseResultsOfLastWeekEnd(html);
+    if (results && results.length > 0) {
+      console.log("Persisting the results");
+      fs.writeFileSync(lastWeekEndResultsFileName, JSON.stringify(results));
+    } else {
+      console.log("Nothing to persist now");
+    }
+  }).catch((error) => {
+    console.log(error);
+  });
+}
